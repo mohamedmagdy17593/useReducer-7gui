1ba6593261e5b3b05dc12b4e480383548b432a32
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-09-18 16:43:21 +0200

    add basic app

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-yellow-400 flex justify-center items-center">
+      <div className="w-full max-w-4xl m-4 p-4 bg-white border-2 border-gray-900 rounded"></div>
+    </div>
+  );
+}
+
+export default Home;
